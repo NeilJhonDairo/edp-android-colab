55a751618b235e3b74aac8567ceb8a55440e0669
Neil Jhon Dairo <njdairo13539@liceo.edu.ph>
2026-07-22 10:48:54 +0800

    Contact Profile screen

--- app/src/main/java/com/example/myapplication/ui/theme/Color.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/Color.kt
@@ -2,10 +2,17 @@ package com.example.myapplication.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val PrimaryLight = Color(0xFF1565C0)
+val OnPrimaryLight = Color(0xFFFFFFFF)
+val PrimaryContainerLight = Color(0xFFD3E4FF)
+val SecondaryLight = Color(0xFF4A6FA5)
+val SurfaceLight = Color(0xFFFAFBFF)
+val OnSurfaceVariantLight = Color(0xFF4A4C4E)
+
+val PrimaryDark = Color(0xFFA0C9FF)
+val OnPrimaryDark = Color(0xFF00325A)
+val PrimaryContainerDark = Color(0xFF104A82)
+val SecondaryDark = Color(0xFFB0C6E8)
+val SurfaceDark = Color(0xFF111318)
+val OnSurfaceVariantDark = Color(0xFFC4C6CC)
