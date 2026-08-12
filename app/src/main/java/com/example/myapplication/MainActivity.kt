f03e9297c28cd7adf8f12f9369b04ba7e4d2d4bd
Neil Jhon Dairo <njdairo13539@liceo.edu.ph>
2026-08-12 10:43:04 +0800

    Lab Activity 8: MVVM user profile app

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,30 +3,18 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import androidx.navigation.toRoute
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
-            val navController = rememberNavController()
-
-            NavHost(navController = navController, startDestination = Home) {
-                composable<Home> {
-                    HomeScreen(onShowGreeting = { typedName ->
-                        navController.navigate(Greeting(userName = typedName))
-                    })
-                }
-                composable<Greeting> { backStackEntry ->
-                    val greeting: Greeting = backStackEntry.toRoute()
-                    GreetingScreen(userName = greeting.userName)
+            MaterialTheme {
+                Surface {
+                    ProfileScreen()
                 }
             }
         }
     }
 }
-
